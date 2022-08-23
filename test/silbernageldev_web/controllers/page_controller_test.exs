77f6c9ff9e9a9cd33bf647908c51a8eeb0b9a27c
Matt <matt@silbernagel.dev>
2022-08-22 21:21:22 -0400

    Fix broken test

--- test/silbernageldev_web/controllers/page_controller_test.exs
+++ test/silbernageldev_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule SilbernageldevWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Matt Silbernagel"
   end
 end
