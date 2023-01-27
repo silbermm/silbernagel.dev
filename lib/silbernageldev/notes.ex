217a668a943121fa9c58bca113c0fdec16274def
Matt <matt@silbernagel.dev>
2023-01-26 22:24:06 -0500

    Start of `NOTES`

---
+++ lib/silbernageldev/notes.ex
@@ -0,0 +1,34 @@
+defmodule Silbernageldev.Notes do
+  @moduledoc """
+  This is what builds my [notes](https://indieweb.org/note)
+  """
+
+  alias Silbernageldev.Notes.Note
+
+  use NimblePublisher,
+    build: Note,
+    from: Application.app_dir(:silbernageldev, "priv/notes/**/*.md"),
+    as: :notes,
+    highlighters: [:makeup_elixir, :makeup_erlang, :makeup_html5, :makeup_eex]
+
+  # The @notes variable is first defined by NimblePublisher.
+  # Let's further modify it by sorting all notes by descending date.
+  @notes Enum.sort_by(@notes, & &1.datetime, {:desc, DateTime})
+         |> then(fn notes ->
+           unless Mix.env() == :dev do
+             Enum.reject(notes, &Map.get(&1, :draft, false))
+           else
+             notes
+           end
+         end)
+
+  @doc "Export all notes"
+  def all_notes, do: @notes
+
+  defmodule NotFoundError, do: defexception([:message, plug_status: 404])
+
+  def get_note_by_timestamp(timestamp) do
+    Enum.find(@notes, &(&1.timestamp == timestamp)) ||
+      raise NotFoundError, "note with timestamp=#{timestamp} not found"
+  end
+end
---
+++ lib/silbernageldev/notes/note.ex
@@ -0,0 +1,14 @@
+defmodule Silbernageldev.Notes.Note do
+  @enforce_keys [:author, :body, :datetime]
+  defstruct [:author, :body, :tags, :datetime, :draft, :reply_to]
+
+  def build(filename, attrs, body) do
+    [year, month_day_time] = filename |> Path.rootname() |> Path.split() |> Enum.take(-2)
+
+    [month, day, time] = String.split(month_day_time, "-", parts: 3)
+    date = Date.from_iso8601!("#{year}-#{month}-#{day}")
+    time = Time.from_iso8601!(time)
+    datetime = DateTime.new!(date, time)
+    struct!(__MODULE__, [datetime: datetime, body: body] ++ Map.to_list(attrs))
+  end
+end
